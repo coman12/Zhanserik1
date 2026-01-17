ceec44928bb864a602fdb2ed7afe6b5ef80af0f8
Zhanserik <253574@astanait.edu.kz>
2026-01-17 11:20:01 +0500

    Assignment 3: PostgreSQL CRUD operations

---
+++ tables.sql
@@ -0,0 +1,21 @@
+-- Assignment 3 – Project Milestone 1
+-- Subject area: E-commerce Platform
+
+CREATE TABLE product (
+                         id SERIAL PRIMARY KEY,
+                         name VARCHAR(100),
+                         price NUMERIC(10,2),
+                         stock INT
+);
+
+INSERT INTO product (name, price, stock)
+VALUES ('Phone', 800, 10);
+
+SELECT * FROM product;
+
+UPDATE product
+SET price = 750
+WHERE id = 1;
+
+DELETE FROM product
+WHERE id = 1;
